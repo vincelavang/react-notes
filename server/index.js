5406a8bdf49a422d58b1d89cac79931080358cba
Vince Lavang <vincelavang@gmail.com>
2017-08-02 14:23:12 -0700

    Added Express route to delete note by id. #5

--- server/index.js
+++ server/index.js
@@ -40,6 +40,19 @@ app.post('/notes', function (req, res) {
     })
 })
 
+app.delete('/notes/:id', function (req, res) {
+  const query = knex
+    .where('id', req.params.id)
+    .from('notes')
+    .delete()
+
+    console.log(query.toString())
+  query
+    .then((response) => {
+      console.log('done!')
+    })
+  })
+
 app.use(staticMiddleware)
 
 app.listen(3000, () => {
