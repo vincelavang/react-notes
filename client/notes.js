b9e694f3f2ff8bd2578c9da9004f1cc22cefbfb0
Vince Lavang <vincelavang@gmail.com>
2017-08-02 15:48:32 -0700

    Enhanced Notes to send DELETE request when clicked. #5

--- client/notes.js
+++ client/notes.js
@@ -5,7 +5,7 @@ function Note(props) {
   return (
     <div className='ui card blue label'>
       <div className='content'>
-        <div className='header'>{note.id}</div>
+        <div onClick= {() => deleteById(note.id)} className='header'>{note.id}</div>
         <div className='meta'>Notes</div>
         <div className='description'>{note.note_text}</div>
       </div>
@@ -13,11 +13,23 @@ function Note(props) {
   )
 }
 
+function deleteById(id) {
+  fetch('/notes/' + id, {
+    method: 'DELETE'
+  })
+    .then(() => {
+      console.log('DELETED!')
+    })
+  }
+
 export default class Notes extends React.Component {
+  handleClick(event) {
+    event.preventDefault()
+  }
   render() {
     return (
       <div>
-        {this.props.notes.map((note, i) => <Note key={i} note={note} />)}
+        {this.props.notes.map((note, i) => <Note click={this.handleClick.bind(this)} key={i} note={note} />)}
       </div>
     )
   }
